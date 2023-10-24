d57faa816c809cbcb3454d3294d295223600fb7d
thomas demoncy <demoncy.thomas@gmail.com>
2023-10-24 16:01:42 +0200

    started card and cards grid components using mantine. stoped because no data atm

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
 import { useState } from 'react'
 import '@mantine/core/styles.css';
 import Navbar from './components/Navbar';
+import EquipmentCard from './components/Card';
+import CardGrid from './components/CardGrid';
 
 function App() {
+  const [equipments, setEquipments] = useState();
+
   return (
     <> 
     <Navbar />
