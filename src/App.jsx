7696b2623c106930f0aa3c4bd5247d58b3c88a1c
mwnCoding <MaximilianoNaddeo@gmail.com>
2023-10-25 18:23:48 +0200

    init create equipment page

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,27 @@
-import '@mantine/core/styles.css';
-import Navbar from './components/Navbar';
-import { Routes, Route } from 'react-router-dom';
-import Homepage from './pages/Homepage';
-import SignupPage from './pages/users/SignUpPage';
-import LoginPage from './pages/users/LogInPage';
+import "@mantine/core/styles.css";
+import Navbar from "./components/Navbar";
+import { Routes, Route } from "react-router-dom";
+import Homepage from "./pages/Homepage";
+import SignupPage from "./pages/users/SignUpPage";
+import LoginPage from "./pages/users/LogInPage";
+import CreateEquipment from "./pages/equipments/CreateEquipmentPage";
 
 function App() {
   return (
-    <> 
-    <Navbar />
-    <Routes>
-      <Route path="/" element={<Homepage/ >}/>
-      <Route path="/login" element={<LoginPage />}/>
-      {/* <Route path='/equipments/:equipmentId' element={} /> */}
-      <Route path="/signup" element={<SignupPage />}/>
-    </Routes>
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/login" element={<LoginPage />} />
+        {/* <Route path='/equipments/:equipmentId' element={} /> */}
+        <Route path="/signup" element={<SignupPage />} />
+        <Route
+          path="/createEquipment/:userId"
+          element={<CreateEquipment />}
+        ></Route>
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/pages/equipments/CreateEquipmentPage.jsx
+++ src/pages/equipments/CreateEquipmentPage.jsx
@@ -1,5 +1,65 @@
+import {
+  Flex,
+  Container,
+  Card,
+  Image,
+  TextInput,
+  Button,
+  Title,
+} from "@mantine/core";
+
+import { useForm } from "@mantine/form";
+
 function CreateEquipment() {
-    
+  const form = useForm({
+    initialValues: {
+      imageUrl: "../src/assets/equipmentPlaceholder.jpg",
+      name: "",
+      description: "",
+      condition: "",
+      categories: [],
+    },
+  });
+
+  console.log(form);
+
+  function uploadImage() {}
+
+  return (
+    <Flex
+      mih={50}
+      gap="md"
+      justify="flex-start"
+      align="center"
+      direction="column"
+      wrap="nowrap"
+    >
+      <Title order={1} fw={900} c="#288BE2" size="52">
+        Add new Equipment
+      </Title>
+      <form action="">
+        <Flex>
+          <Card>
+            <Card.Section>
+              <Image
+                src={form.getInputProps("imageUrl").value}
+                w="10em"
+                h="10em"
+                radius="md"
+              />
+            </Card.Section>
+            <TextInput label="Image Url" placeholder="Enter Image Url" />
+            <Button variant="filled" onClick={uploadImage}>
+              Upload
+            </Button>
+          </Card>
+          <Container id="form-container">
+            <TextInput label="" />
+          </Container>
+        </Flex>
+      </form>
+    </Flex>
+  );
 }
 
 export default CreateEquipment;
