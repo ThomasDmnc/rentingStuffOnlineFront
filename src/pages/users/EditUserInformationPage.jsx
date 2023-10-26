24d238e9b5d24f3f40d891bdb9279d29499b7491
aydinhalil21 <aydinhalil21@gmail.com>
2023-10-26 22:21:31 +0200

    upload button

--- src/pages/users/EditUserInformationPage.jsx
+++ src/pages/users/EditUserInformationPage.jsx
@@ -1,30 +1,27 @@
 import React, { useState, useEffect, useContext } from 'react';
-import { useParams, useNavigate} from 'react-router-dom'; 
+import { useParams, useNavigate } from 'react-router-dom';
 import axios from 'axios';
-import { Container, Paper, Image, Text, Grid, TextInput, Button } from '@mantine/core';
+import { Container, Paper, Image, Grid, TextInput, Button } from '@mantine/core';
 import { AuthContext } from '../../contexts/AuthContext';
 
-
-
 const EditUserInformationPage = () => {
-  const { id } = useParams(); 
+  const { id } = useParams();
   const { user } = useContext(AuthContext);
   const [userId, setUserId] = useState(user.userId);
-  const [userData, setUserData] = useState(null); 
+  const [userData, setUserData] = useState(null);
   const navigate = useNavigate();
-
-  
+  const [file, setFile] = useState(null);
 
   useEffect(() => {
     axios.get(`${import.meta.env.VITE_API_URL}/api/user/${userId}`).then((response) => {
-      setUserData(response.data); 
+      setUserData(response.data);
     });
-  }, [userId]); 
+  }, [userId]);
 
   const handleInputChange = (e) => {
     const { name, value } = e.target;
     setUserData({
-      ...userData, 
+      ...userData,
       [name]: value,
     });
   };
@@ -32,22 +29,38 @@ const EditUserInformationPage = () => {
   const handleSaveChanges = () => {
     const updatedUserData = {
       imageUrl: userData?.imageUrl,
-      firstName: userData?.firstName, 
-      lastName: userData?.lastName, 
-      email: userData?.email, 
+      firstName: userData?.firstName,
+      lastName: userData?.lastName,
+      email: userData?.email,
     };
 
     axios
       .put(`${import.meta.env.VITE_API_URL}/api/user/${userId}`, updatedUserData)
       .then((response) => {
-       
         console.log('User data updated successfully');
         navigate('/profile');
-      
-        
       })
       .catch((error) => {
         console.error('Error updating user data:', error);
+      });
+  };
+
+  const handleUpdateImage = (e) => {
+    const selectedFile = e.target.files[0];
+    setFile(selectedFile);
+  };
+
+  const handleUploadImage = () => {
+    const formData = new FormData();
+    formData.append('imageUrl', file);
+
+    axios
+      .post(`${import.meta.env.VITE_API_URL}/api/users/upload`, formData)
+      .then((response) => {
+        setUserData({ ...userData, imageUrl: response.data.fileUrl });
+      })
+      .catch((error) => {
+        console.error('Error updating user image:', error);
       });
   };
 
@@ -57,26 +70,37 @@ const EditUserInformationPage = () => {
         <Grid gutter="md">
           <Grid.Col span={3}>
             {userData ? (
-              <Image src={userData.imageUrl} width={150} height={150} alt={`${userData.firstName} ${userData.lastName}`} />
+              <div>
+                <Image
+                  src={userData.imageUrl}
+                  width={150}
+                  height={150}
+                  alt={`${userData.firstName} ${userData.lastName}`}
+                />
+                <input type="file" accept="image/*" onChange={handleUpdateImage} />
+                <Button variant="filled" onClick={handleUploadImage}>
+                  Update Image
+                </Button>
+              </div>
             ) : null}
           </Grid.Col>
           <Grid.Col span={9}>
             <TextInput
               name="firstName"
               label="First Name"
-              value={userData?.firstName || ''} 
+              value={userData?.firstName || ''}
               onChange={handleInputChange}
             />
             <TextInput
               name="lastName"
               label="Last Name"
-              value={userData?.lastName || ''} 
+              value={userData?.lastName || ''}
               onChange={handleInputChange}
             />
             <TextInput
               name="email"
               label="Email"
-              value={userData?.email || ''} 
+              value={userData?.email || ''}
               onChange={handleInputChange}
             />
             <Button variant="filled" onClick={handleSaveChanges}>
