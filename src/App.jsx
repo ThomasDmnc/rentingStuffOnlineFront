f8d80f55f50bc83a5a2c6a2faefc589dc316bfba
mwnCoding <MaximilianoNaddeo@gmail.com>
2023-10-26 17:28:56 +0200

    finish create equipment page

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Homepage from "./pages/Homepage";
 import SignupPage from "./pages/users/SignUpPage";
 import LoginPage from "./pages/users/LogInPage";
 import CreateEquipment from "./pages/equipments/CreateEquipmentPage";
+import PrivateRoute from "./components/PrivateRoute";
 
 function App() {
   return (
@@ -16,8 +17,12 @@ function App() {
         {/* <Route path='/equipments/:equipmentId' element={} /> */}
         <Route path="/signup" element={<SignupPage />} />
         <Route
-          path="/createEquipment/:userId"
-          element={<CreateEquipment />}
+          path="/createEquipment"
+          element={
+            <PrivateRoute>
+              <CreateEquipment />
+            </PrivateRoute>
+          }
         ></Route>
       </Routes>
     </>
--- src/pages/equipments/CreateEquipmentPage.jsx
+++ src/pages/equipments/CreateEquipmentPage.jsx
@@ -6,24 +6,69 @@ import {
   TextInput,
   Button,
   Title,
+  Textarea,
+  MultiSelect,
+  Select,
+  FileInput,
 } from "@mantine/core";
 
-import { useForm } from "@mantine/form";
+import { AuthContext } from "../../contexts/AuthContext";
+import { useContext, useState } from "react";
+
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 function CreateEquipment() {
-  const form = useForm({
-    initialValues: {
-      imageUrl: "../src/assets/equipmentPlaceholder.jpg",
-      name: "",
-      description: "",
-      condition: "",
-      categories: [],
-    },
-  });
+  const navigate = useNavigate();
 
-  console.log(form);
+  const { user } = useContext(AuthContext);
 
-  function uploadImage() {}
+  const [imageUrl, setImageUrl] = useState(
+    "../src/assets/equipmentPlaceholder.jpg"
+  );
+  const [name, setName] = useState("");
+  const [description, setDescription] = useState("");
+  const [condition, setCondition] = useState("");
+  const [categories, setCategories] = useState();
+  const [ownedBy, setOwnedBy] = useState(user.userId);
+
+  function uploadImage() {
+    //Upload on cloudymoudy and set imageUrl to cloud Url
+    console.log("Uploading image");
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    const payload = {
+      name,
+      description,
+      imageUrl,
+      condition,
+      categories,
+      ownedBy,
+    };
+    console.log(payload);
+    axios
+      .post(
+        `${import.meta.env.VITE_API_URL}/api/equipments`,
+        JSON.stringify(payload),
+        {
+          headers: {
+            "Content-Type": "application/json",
+            "Access-Control-Allow-Origin": true,
+          },
+        }
+      )
+      .then((response) => {
+        console.log(response);
+        //Change to navigate to user equipment page
+        navigate("/");
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
 
   return (
     <Flex
@@ -32,29 +77,68 @@ function CreateEquipment() {
       justify="flex-start"
       align="center"
       direction="column"
-      wrap="nowrap"
+      wrap="wrap"
     >
       <Title order={1} fw={900} c="#288BE2" size="52">
         Add new Equipment
       </Title>
-      <form action="">
-        <Flex>
+      <form onSubmit={handleSubmit}>
+        <Flex wrap="wrap" justify="center">
           <Card>
             <Card.Section>
-              <Image
-                src={form.getInputProps("imageUrl").value}
-                w="10em"
-                h="10em"
-                radius="md"
+              <Image src={imageUrl} w="10em" h="10em" radius="md" />
+              <FileInput
+                label="Equipment image"
+                placeholder="Enter Image Url"
               />
+              <Button variant="filled" onClick={uploadImage}>
+                Upload
+              </Button>
             </Card.Section>
-            <TextInput label="Image Url" placeholder="Enter Image Url" />
-            <Button variant="filled" onClick={uploadImage}>
-              Upload
-            </Button>
           </Card>
           <Container id="form-container">
-            <TextInput label="" />
+            <TextInput
+              label="Name"
+              placeholder="Enter Equipment name"
+              value={name}
+              onChange={(event) => {
+                setName(event.currentTarget.value);
+              }}
+            />
+            <Textarea
+              label="description"
+              placeholder="Enter Equipment description"
+              value={description}
+              onChange={(event) => {
+                setDescription(event.currentTarget.value);
+              }}
+            />
+            <Flex wrap={"wrap"}>
+              <Select
+                label="Condition"
+                placeholder="Choose condition"
+                data={["poor", "used", "good", "new"]}
+                value={condition}
+                onChange={setCondition}
+              />
+              <MultiSelect
+                label="Categories"
+                data={[
+                  "Tennis",
+                  "Climbing",
+                  "Fishing",
+                  "Hiking",
+                  "Surfing",
+                  "Biking",
+                  "Skiing",
+                ]}
+                value={categories}
+                onChange={setCategories}
+                hidePickedOptions
+                searchable
+              />
+            </Flex>
+            <Button type="submit">Add Equipment</Button>
           </Container>
         </Flex>
       </form>
