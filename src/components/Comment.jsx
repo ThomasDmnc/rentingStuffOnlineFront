4b90646b7724e58356a2665219e61b2236a8e5e8
thomas demoncy <demoncy.thomas@gmail.com>
2023-10-27 16:17:57 +0200

    added the delete method in wrong component to make it work correctly

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,24 +1,13 @@
-import { useState, useContext } from "react";
-import { Text, Rating, Flex, Avatar, Button } from "@mantine/core";
-import { Link } from 'react-router-dom';
-import { AuthContext } from "../contexts/AuthContext.jsx";
-import axios from "axios";
+import { useState } from "react";
+import { Text, Rating, Flex, Avatar } from "@mantine/core";
+
 
 function Comment({ theComment }) {
   const [comment, setComment] = useState(theComment);
-  const { user } = useContext(AuthContext);
 
-  const handleDelete = (commentId) => {
-    axios.delete(`${import.meta.env.VITE_API_URL}/api/comments/${commentId}`)
-    .then((response) => {
-      console.log(response)
-    }).catch((err) => {console.log(err)})
-  }
-
-  console.log(user);
   return (
     <>
-      <Flex mt={20} mb={20} 
+      <Flex w="100%"  mt={20} mb={20} 
         direction={{ base: 'column', sm: 'row' }}
         gap={{ base: 'sm', sm: 'lg' }}
       >
@@ -28,25 +17,10 @@ function Comment({ theComment }) {
             {comment.createdBy.firstName} {comment.createdBy.lastName}
           </Text>
         </Flex>
-        <Flex w="60%" direction="column" justify="space-between" mr={10}>
+        <Flex w="100%" direction="column" justify="space-between" mr={10}>
           <Rating value={comment.rating} />
           <Text mt={20}>{comment.content}</Text>
         </Flex>
-        { user.userId === comment.createdBy._id ? (
-            <>
-            <Flex
-              direction="column"
-              align="center"
-            >
-                <Button w="100" component={Link} to={`/updateComment/${comment._id}`}  variant="filled" color="#288BE2" size="sm">
-                    Edit
-                </Button>
-                <Button w="100" mt={10} onClick={() => handleDelete(comment._id)} variant="filled" color="#CA1747" size="sm">
-                    Delete
-                </Button>
-                </Flex>
-            </>
-        ) : ""}
       </Flex>
     </>
   );
--- src/components/CommentGrid.jsx
+++ src/components/CommentGrid.jsx
@@ -1,10 +1,28 @@
-import { useEffect, useState } from "react";
-import { Grid } from '@mantine/core';
+import { useEffect, useState, useContext } from "react";
+import { Grid, Flex, Button } from '@mantine/core';
 import Comment from "./Comment";
+import { AuthContext } from "../contexts/AuthContext.jsx";
+import axios from "axios";
+import { Link } from "react-router-dom";
 
 function CommentGrid({ allcomments }) {
     const [comments, setComments] = useState()
     const [isLoading, setIsLoading ] = useState(false)
+
+    const { user } = useContext(AuthContext);
+  
+    const handleDelete = (comment) => {
+      axios.delete(`${import.meta.env.VITE_API_URL}/api/comments/${comment._id}`)
+      .then((response) => {
+        console.log(response)
+        const newComments = comments.filter(function (el) {
+            return el != comment;
+          })
+        setComments(newComments);
+      }).catch((err) => {console.log(err)})
+    }
+  
+    console.log(user);
 
     useEffect(() =>{
         setComments(allcomments)
@@ -17,8 +35,24 @@ function CommentGrid({ allcomments }) {
          <Grid gutter="lg"  spacing="lg" mt={20}>
             { typeof comments === 'object' && comments.length > 0 && comments.map((comment) => {
                 return(
-                    <Grid.Col key={comment.id} span={{ base: 12, xs:12, md: 12, lg: 12}}>
+                    <Grid.Col key={comment._id} span={{ base: 12, xs:12, md: 12, lg: 12}}>
+                        <Flex w='100%'>
                         <Comment theComment={comment} />
+                        { user.userId === comment.createdBy._id ? (
+                            <Flex
+                            direction="column"
+                            align="center"
+                            justify="center"
+                            >
+                                <Button w="100" component={Link} to={`/updateComment/${comment._id}`}  variant="filled" color="#288BE2" size="sm">
+                                    Edit
+                                </Button>
+                                <Button w="100" mt={10} onClick={() => handleDelete(comment)} variant="filled" color="#CA1747" size="sm">
+                                    Delete
+                                </Button>
+                                </Flex>
+                        ) : ""}
+                        </Flex>
                     </Grid.Col>
                 )
             })}
