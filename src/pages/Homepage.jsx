ed1c030f6cb3bf88142f119e32cc7f303aaa13bb
mwnCoding <MaximilianoNaddeo@gmail.com>
2023-11-01 17:40:31 +0100

    made search functional

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,131 +1,191 @@
 import { useState, useEffect } from "react";
 import CardGrid from "../components/CardGrid";
 import axios from "axios";
-import { Title, Flex, Text, Button, Input, Menu, MenuItem, Pagination, rem } from '@mantine/core';
+import {
+  Title,
+  Flex,
+  Text,
+  Button,
+  Input,
+  Menu,
+  MenuItem,
+  Pagination,
+  rem,
+} from "@mantine/core";
 
 function Homepage() {
-    const [equipments, setEquipments] = useState('');
-    const [selectedTag, setSelectedTag] = useState(null);
-    const [searchTerm, setSearchTerm] = useState('');
-    const [currentPage, setCurrentPage] = useState(1);
-    const [itemsPerPage, setItemsPerPage] = useState(10); 
-   
+  const [equipments, setEquipments] = useState("");
+  const [filteredEquipments, setFilteredEquipments] = useState("");
+  const [selectedTag, setSelectedTag] = useState(null);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [currentPage, setCurrentPage] = useState(1);
+  const [itemsPerPage, setItemsPerPage] = useState(10);
 
-    const getEquipments = () => {
-        let url = `${import.meta.env.VITE_API_URL}/api/equipments?available=true`;
+  const getEquipments = () => {
+    let url = `${import.meta.env.VITE_API_URL}/api/equipments?available=true`;
 
-        const queryParams = [];
+    const queryParams = [];
 
-        if (selectedTag) {
-            queryParams.push(`categories=${selectedTag}`);
-        }
-        if (searchTerm) {
-            queryParams.push(`search=${searchTerm}`);
-        }
-
-        if (queryParams.length > 0) {
-            url += `&${queryParams.join('&')}`;
-        }
-
-        axios.get(url)
-            .then((response) => {
-                setEquipments(response.data);
-            })
-            .catch((error) => {
-                console.log(error);
-            });
+    if (selectedTag) {
+      queryParams.push(`categories=${selectedTag}`);
     }
 
-    useEffect(() => {
-        getEquipments();
-    }, [selectedTag, searchTerm]);
+    if (queryParams.length > 0) {
+      url += `&${queryParams.join("&")}`;
+      console.log(url);
+    }
 
+    axios
+      .get(url)
+      .then((response) => {
+        setEquipments(response.data);
+        handleSearch();
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
 
-    const handleTagFilter = (tag) => {
-        setSelectedTag(tag);
-        setSearchTerm('');
-        setCurrentPage(1);
-    };
+  useEffect(() => {
+    getEquipments();
+    setFilteredEquipments(equipments);
+  }, []);
 
-    const allTags = [
-        "Tennis",
-        "Climbing",
-        "Fishing",
-        "Hiking",
-        "Surfing",
-        "Biking",
-        "Skiing"
-    ];
+  useEffect(() => {
+    getEquipments();
+  }, [selectedTag]);
 
-    const handleSearch = (value) => {
-        setSearchTerm(value);
-        setSelectedTag(null);
-        setCurrentPage(1);
-    };
+  useEffect(() => {
+    handleSearch();
+  }, [searchTerm]);
 
-    const indexOfLastItem = currentPage * itemsPerPage;
-    const indexOfFirstItem = indexOfLastItem - itemsPerPage;
-    const currentItems = equipments.slice(indexOfFirstItem, indexOfLastItem);
+  useEffect(() => {
+    handleSearch();
+  }, [equipments]);
 
-    const paginate = (pageNumber) => setCurrentPage(pageNumber);
+  const handleTagFilter = (tag) => {
+    setSelectedTag(tag);
+    setCurrentPage(1);
+  };
 
-    return (
-        <>
-            <section className="banner">
-                <Flex
-                    direction="column"
-                    align="center"
-                    justify="center"
-                    mih="40vh"
-                >
-                    <Title order={1} fw={900} c="#288BE2" size="52">Renting Stuff Online</Title>
-                    <Text mt="2rem" fw={600}>The place where you get what you want</Text>
-                </Flex>
-            </section>
+  const allTags = [
+    "Tennis",
+    "Climbing",
+    "Fishing",
+    "Hiking",
+    "Surfing",
+    "Biking",
+    "Skiing",
+  ];
 
-            <section className="searchBar" style={{ margin: '20px 0' }}>
-                <Input
-                    placeholder="Search by name or description"
-                    value={searchTerm}
-                    onChange={(event) => handleSearch(event.currentTarget.value)}
-                    variant="default"
-                />
-            </section>
+  const handleSearch = () => {
+    console.log(equipments);
+    console.log(filteredEquipments);
+    console.log(searchTerm);
+    if (equipments) {
+      const regex = new RegExp(`.*${searchTerm}.*`, "i");
+      console.log(regex);
+      const tempFilteredEquipments = equipments.filter((currentEquipment) => {
+        return regex.test(currentEquipment.name);
+      });
+      console.log(tempFilteredEquipments);
+      setFilteredEquipments(tempFilteredEquipments);
+    }
+  };
 
-            <section className="tagFilters" style={{ margin: '20px 0' }}>
-                <Flex wrap="wrap" justify="center" gap="md">
-                    <Button onClick={() => handleTagFilter(null)}>All</Button>
-                    {allTags.map(tag => (
-                        <Button key={tag} onClick={() => handleTagFilter(tag)}>{tag}</Button>
-                    ))}
-                </Flex>
-            </section>
+  const indexOfLastItem = currentPage * itemsPerPage;
+  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+  const currentItems = filteredEquipments.slice(
+    indexOfFirstItem,
+    indexOfLastItem
+  );
 
-            
-            <section className="gridCtn">
-                <CardGrid allEquipments={currentItems} />
-            </section>
-            <section className="paginationOptions" style={{ textAlign: "center", margin: "20px 0" }}>
-                <Menu>
-                    <Menu.Target>
-                        <Button>Item Per Page ({itemsPerPage}) </Button>
-                    </Menu.Target>
-                    <Menu.Dropdown>
-                    <MenuItem onClick={() => {setItemsPerPage(10),setCurrentPage(1)}}>10</MenuItem>
-                    <MenuItem onClick={() => {setItemsPerPage(25),setCurrentPage(1)}}>25</MenuItem>
-                    <MenuItem onClick={() => {setItemsPerPage(50),setCurrentPage(1)}}>50</MenuItem>
-                    </Menu.Dropdown>
-                </Menu>
-            </section>
+  const paginate = (pageNumber) => setCurrentPage(pageNumber);
 
+  return (
+    <>
+      <section className="banner">
+        <Flex direction="column" align="center" justify="center" mih="40vh">
+          <Title order={1} fw={900} c="#288BE2" size="52">
+            Renting Stuff Online
+          </Title>
+          <Text mt="2rem" fw={600}>
+            The place where you get what you want
+          </Text>
+        </Flex>
+      </section>
 
-            <section className="pagination" style={{ textAlign: "center", margin: "20px 0" }}>
-                <Flex justify="center" align="center" mt="2rem">
-                <Pagination value={currentPage} onChange={setCurrentPage}  total={Math.ceil(equipments.length / itemsPerPage)} />
-                </Flex>
-            </section>
-        </>
-    );
+      <section className="searchBar" style={{ margin: "20px 0" }}>
+        <Input
+          placeholder="Search by name or description"
+          value={searchTerm}
+          onChange={(event) => setSearchTerm(event.currentTarget.value)}
+          variant="default"
+        />
+      </section>
+
+      <section className="tagFilters" style={{ margin: "20px 0" }}>
+        <Flex wrap="wrap" justify="center" gap="md">
+          <Button onClick={() => handleTagFilter(null)}>All</Button>
+          {allTags.map((tag) => (
+            <Button key={tag} onClick={() => handleTagFilter(tag)}>
+              {tag}
+            </Button>
+          ))}
+        </Flex>
+      </section>
+
+      <section className="gridCtn">
+        <CardGrid allEquipments={currentItems} />
+      </section>
+      <section
+        className="paginationOptions"
+        style={{ textAlign: "center", margin: "20px 0" }}
+      >
+        <Menu>
+          <Menu.Target>
+            <Button>Item Per Page ({itemsPerPage}) </Button>
+          </Menu.Target>
+          <Menu.Dropdown>
+            <MenuItem
+              onClick={() => {
+                setItemsPerPage(10), setCurrentPage(1);
+              }}
+            >
+              10
+            </MenuItem>
+            <MenuItem
+              onClick={() => {
+                setItemsPerPage(25), setCurrentPage(1);
+              }}
+            >
+              25
+            </MenuItem>
+            <MenuItem
+              onClick={() => {
+                setItemsPerPage(50), setCurrentPage(1);
+              }}
+            >
+              50
+            </MenuItem>
+          </Menu.Dropdown>
+        </Menu>
+      </section>
+
+      <section
+        className="pagination"
+        style={{ textAlign: "center", margin: "20px 0" }}
+      >
+        <Flex justify="center" align="center" mt="2rem">
+          <Pagination
+            value={currentPage}
+            onChange={setCurrentPage}
+            total={Math.ceil(filteredEquipments.length / itemsPerPage)}
+          />
+        </Flex>
+      </section>
+    </>
+  );
 }
 
 export default Homepage;
